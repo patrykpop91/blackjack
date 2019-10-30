6f2418705b6ac748a30520475fae298164ac2226
Popinski <p.popinski@accenture.com>
2019-10-30 14:50:24 +0100

    add blackjack files

---
+++ script.js
@@ -0,0 +1,85 @@
+// Dziwactwa JS
+
+//
+//  1*************
+//
+let value = 4.1 + 4.3;
+
+// console.log(value); // 8.399999999999999
+
+//
+//  2*************
+//
+let value1 = 10 / 0;
+
+// console.log(value1); // Infinity
+
+//
+//  3*************
+//
+let value2 = -10 / 0;
+
+// console.log(value2);  // -Infinity
+
+//
+//  4*************
+//
+let value3 = 0 / 0;
+
+// console.log(value3);  //Nan
+
+//
+//  5*************
+//
+let value4 = 0 / 0;
+
+// console.log(value4, typeof(value4)); //NaN "number" 
+
+//
+//Array Features:
+// 
+
+//  1.push dodaje element do tablicy jako ostatni element
+
+// let array = [];
+// array.push(44);
+// array.push(33);
+// array.push(-100);
+
+// console.log(array);
+
+
+//  2.pop usuwa ostatni element z tablicy
+
+// let array2 = [1, 2, 3, 4];
+// console.log(array2.pop()); // 4
+// console.log(array2);
+
+
+
+//  3.shift usuwa pierwszy element z tblicy
+
+// let array3 = [1, 2, 3, 4];
+// console.log(array3.shift()); 
+// console.log(array3); 
+
+
+//  3.splice usuwa elementy z tablicy, które okreslimy przy wywołaniu funkcji
+// array.splice(a, b) -> a jest tutaj numerem indexu, od którego chcemy zacząć usuwac elementy, 
+// b jest iliscia elementow, ktore zostana usuniete po zdefiniowanym indexie w zmiennej a
+
+// let array4 = [1, 12, 33, 444];
+// console.log(array4.splice(0, 5)); 
+// console.log(array4);  
+
+
+
+
+
+
+
+
+
+
+
+
